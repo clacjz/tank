3162a9e44700a556d47da45a5862d4cec664e0db
clacjz <caixh@163.com>
2020-07-19 19:34:33 +0800

    update

---
+++ src/com/clacjz/tank/Dir.java
@@ -0,0 +1,5 @@
+package com.clacjz.tank;
+
+public enum Dir {
+    LEFT, UP, RIGHT, DOWN
+}
--- src/com/clacjz/tank/TankFrame.java
+++ src/com/clacjz/tank/TankFrame.java
@@ -10,7 +10,7 @@ import java.awt.event.WindowEvent;
  * @author clacjz
  */
 public class TankFrame extends Frame {
-    public TankFrame(){
+    public TankFrame() {
         this.setTitle("Tank War");
         this.setSize(800, 600);
         this.setVisible(true);
@@ -27,25 +27,94 @@ public class TankFrame extends Frame {
     }
 
     int x = 200, y = 200;
+    final int speed;
+    Dir dir = null;
 
     @Override
     public void paint(Graphics g) {
         g.fillRect(x, y, 50, 50);
-        x += 10;
-//        y += 10;
+
+        switch (dir){
+            case UP:
+                y -= speed;
+                break;
+            case DOWN:
+                y += speed;
+                break;
+            case LEFT:
+                x -= speed;
+                break;
+            case RIGHT:
+                x += speed;
+                break;
+            default:
+                break;
+        }
     }
 
-    class MyKeyListener extends KeyAdapter{
+    class MyKeyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bR = false;
+        boolean bU = false;
+        boolean bD = false;
+        private void setMainTankDir(){
+            if (bL){
+                dir = Dir.LEFT;
+            }
+            if (bU){
+                dir = Dir.UP;
+            }
+            if (bR){
+                dir = Dir.RIGHT;
+            }
+            if (bD){
+                dir = Dir.DOWN;
+            }
+        }
         @Override
         public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
 //            x += 30;
+            setMainTankDir();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key release");
+//            System.out.println("key release");
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                default:
+                    break;
+            }
+            setMainTankDir();
         }
     }
-
-
 }
